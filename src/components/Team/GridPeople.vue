eb02393ca6fa5c035d262a91dbaa02c909c5465a
Tom <martin07tom@gmail.com>
2023-01-19 23:50:40 +0100

    created GridPeople.vue created Team.vue

---
+++ src/components/Team/GridPeople.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="grid-people">
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/_colors.scss';
+@use '@/assets/styles/_fonts.scss';
+
+</style>
---
+++ src/views/Team.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="team">
+    <Navbar />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Team',
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.projects {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .navbar {
+    position: fixed;
+    top: 3rem;
+  }
+}
+
+</style>
